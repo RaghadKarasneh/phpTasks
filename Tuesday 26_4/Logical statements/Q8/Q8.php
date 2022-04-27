9546b9cb70a647a29e61e66665174539b57c17e0
Raghad Karasneh <raghad.karasneh@gmail.com>
2022-04-27 16:01:29 +0300

    PHP Tasks

--- Tuesday 26_4/Logical statements/Q8/Q8.php
+++ Tuesday 26_4/Logical statements/Q8/Q8.php
@@ -3,11 +3,11 @@
 $units=$_POST['number'];
 
 if($units<=50)
-    echo 'The monthly electricity bill is 2.50JOD/Unit';
+    echo 'The monthly electricity bill is '. $units* 2.50 .' JOD/Unit';
 else if ($units>50 && $units<=150)
-    echo 'The monthly electricity bill is 5.00JOD/Unit';
+    echo 'The monthly electricity bill is '. $units* 5.00 .' JOD/Unit';
 else if ($units>150 && $units<=250)
-    echo 'The monthly electricity bill is 6.20JOD/Unit';
+    echo 'The monthly electricity bill is '. $units*  6.20 .' JOD/Unit';
 else if ($units>250)
-    echo 'The monthly electricity bill is 7.50JOD/Unit';
+    echo 'The monthly electricity bill is '. $units*  7.50 .' JOD/Unit';
 ?>
