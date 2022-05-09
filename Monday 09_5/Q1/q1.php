6c3358c52e098032a45abd42210007ed08cf118b
Raghad Karasneh <raghad.karasneh@gmail.com>
2022-05-09 15:31:30 +0300

    PHP Tasks

--- Monday 09_5/Q1/q1.php
+++ Monday 09_5/Q1/q1.php
@@ -1,6 +1,5 @@
 <?php
 $city=$_POST['city'];
 echo "Your favorite city is ". $city;
-
 $info=array($city);
 ?>
---
+++ Monday 09_5/Q2/q2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Q2</title>
+</head>
+<body>
+    <form method="post">
+        <p>Please insert you favorite city</p>
+        <input type="text" name="city">
+        <br>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</body>
+</html>
+<?php
+if(isset($_POST['submit']))
+{
+    echo "Your favorite city is ". $_POST['city'];
+    $info=array($_POST['city']);
+}
+?>
